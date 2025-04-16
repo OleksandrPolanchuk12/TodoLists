e91dafb2bba0b98f1b1ceb60f5295bdb2bfa2978
OleksandrPolanchuk12 <polanchykoleksandr@gmail.com>
2025-04-16 10:27:41 +0300

    test3

--- todolist/settings.py
+++ todolist/settings.py
@@ -160,6 +160,5 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
