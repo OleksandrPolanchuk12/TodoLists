73f18d7c715e43b9ce6e51bf45cb73fc22643e6e
OleksandrPolanchuk12 <polanchykoleksandr@gmail.com>
2025-04-16 10:07:24 +0300

    test-release

--- todolist/settings.py
+++ todolist/settings.py
@@ -151,9 +151,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
 STATIC_URL = 'static/'
-
 
 STATICFILES_DIRS = [
     BASE_DIR / 'todolist' / "static",
